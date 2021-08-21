4e60698ae1367b6ff62911aba0e6d8e8a2742cda
Wing Wong <wingedpower@gmail.com>
2021-08-20 23:39:45 -0700

    sync

--- libs/wtc_7seg.v
+++ libs/wtc_7seg.v
@@ -1,128 +1,105 @@
 // Wing Tech Corner - 7 segment control library
 module wtc_7seg
   (
-   input       i_Clk,
-   input [2:0] i_mode = 0,
-   input [3:0] i_Binary_Num = 4'b0000,
-   output      o_Segment_A,
-   output      o_Segment_B,
-   output      o_Segment_C,
-   output      o_Segment_D,
-   output      o_Segment_E,
-   output      o_Segment_F,
-   output      o_Segment_G
+   input  i_Clk,
+   input  [2:0]  i_mode       = 3'b000,
+   input  [4:0]  i_value      = 5'b00000,
+   output [6:0]  o_7segment   = 7'b0000000,
    );
 
-  reg [31:0]   r_Segment_A_period = 1;
-  reg [31:0]   r_Segment_B_period = 1;
-  reg [31:0]   r_Segment_C_period = 1;
-  reg [31:0]   r_Segment_D_period = 1;
-  reg [31:0]   r_Segment_E_period = 1;
-  reg [31:0]   r_Segment_F_period = 1;
-  reg [31:0]   r_Segment_G_period = 1;
+  // For determining 
+  reg [31:0]  period  = 1200;
+  reg [31:0]  counter = 0;
+  reg [3:0]   mode    = 0;
 
-  reg [31:0]   r_Segment_A_counter = 0;
-  reg [31:0]   r_Segment_B_counter = 0;
-  reg [31:0]   r_Segment_C_counter = 0;
-  reg [31:0]   r_Segment_D_counter = 0;
-  reg [31:0]   r_Segment_E_counter = 0;
-  reg [31:0]   r_Segment_F_counter = 0;
-  reg [31:0]   r_Segment_G_counter = 0;
-
-  reg [6:0]    r_Hex_Encoding = 7'h00;
-  reg [6:0]    r_Hex_Encoding_state = 7'h00;
+  // Modes:
+  // 000 => set the display mode using i_value
+  // 001 => RESET
+  // 110 => set the brightness period divisor
+  // 111 => set the brightness period divisor
 
   always @(posedge i_Clk)
   begin
+    case (i_mode)
 
-    // ***************************************************
+      3'b000 : begin
+        // Default case, set the mode of display.
+        // Uses the first 4 bits to set the mode.
+        mode <= i_value[3:0];
+      end
 
-    // Handle Segment A 
-    if ( r_Segment_A_counter > r_Segment_A_peripd )
-    begin
-      r_Hex_Encoding_state[6] <= ~r_Hex_Encoding_state[6];
-      r_Segment_A_counter <= 0;
-    end
-    else
-    begin
-      r_Segment_A_counter <= r_Segment_A_counter + 1;
-    end
+      3'b001 : begin
+        // Reset
+        period    <= 1200;
+        count     <= 0;
+        mode      <= 0;
+      end
 
-    // Handle Segment B 
-    if ( r_Segment_B_counter > r_Segment_B_peripd )
-    begin
-      r_Hex_Encoding_state[5] <= ~r_Hex_Encoding_state[5];
-      r_Segment_B_counter <= 0;
-    end
-    else
-    begin
-      r_Segment_B_counter <= r_Segment_B_counter + 1;
-    end
+      3'b010      : begin
+        // Default case, set the digit
+        case      ( i_value )
+          4'b000  : begin
+          end
+          default : begin
+          end
+        endcase
+      end
 
-    // Handle Segment C 
-    if ( r_Segment_C_counter > r_Segment_C_peripd )
-    begin
-      r_Hex_Encoding_state[4] <= ~r_Hex_Encoding_state[4];
-      r_Segment_C_counter <= 0;
-    end
-    else
-    begin
-      r_Segment_C_counter <= r_Segment_C_counter + 1;
-    end
+      3'b001      : begin
+        // Default case, set the digit
+        case      ( i_value )
+          4'b000  : begin
+          end
+          default : begin
+          end
+        endcase
+      end
 
-    // Handle Segment D 
-    if ( r_Segment_D_counter > r_Segment_D_peripd )
-    begin
-      r_Hex_Encoding_state[3] <= ~r_Hex_Encoding_state[3];
-      r_Segment_D_counter <= 0;
-    end
-    else
-    begin
-      r_Segment_D_counter <= r_Segment_D_counter + 1;
-    end
+      3'b011      : begin
+        // Default case, set the digit
+        case      ( i_value )
+          4'b000  : begin
+          end
+          default : begin
+          end
+        endcase
+      end
 
-    // Handle Segment E 
-    if ( r_Segment_E_counter > r_Segment_E_peripd )
-    begin
-      r_Hex_Encoding_state[2] <= ~r_Hex_Encoding_state[2];
-      r_Segment_E_counter <= 0;
-    end
-    else
-    begin
-      r_Segment_E_counter <= r_Segment_E_counter + 1;
-    end
+      3'b101      : begin
+        // Default case, set the digit
+        case ( i_value )
+          4'b000 : begin
+          end
+          default : begin
+          end
+        endcase
+      end
 
-    // Handle Segment F 
-    if ( r_Segment_F_counter > r_Segment_F_peripd )
-    begin
-      r_Hex_Encoding_state[1] <= ~r_Hex_Encoding_state[1];
-      r_Segment_F_counter <= 0;
-    end
-    else
-    begin
-      r_Segment_F_counter <= r_Segment_F_counter + 1;
-    end
+      3'b110 : begin
+        // Default case, set the digit
+        case ( i_value )
+          4'b000 : begin
+          end
+          default : begin
+          end
+        endcase
+      end
 
-    // Handle Segment G 
-    if ( r_Segment_G_counter > r_Segment_G_peripd )
-    begin
-      r_Hex_Encoding_state[0] <= ~r_Hex_Encoding_state[0];
-      r_Segment_G_counter <= 0;
-    end
-    else
-    begin
-      r_Segment_G_counter <= r_Segment_G_counter + 1;
-    end
+      3'b101 : begin
+        // Default case, set the digit
+        case ( i_value )
+          4'b000 : begin
+          end
+          default : begin
+          end
+        endcase
+      end
 
-    // ***************************************************
-
-    r_Segment_A <= r_Hex_Encoding_state[6];
-    r_Segment_B <= r_Hex_Encoding_state[5];
-    r_Segment_C <= r_Hex_Encoding_state[4];
-    r_Segment_D <= r_Hex_Encoding_state[3];
-    r_Segment_E <= r_Hex_Encoding_state[2];
-    r_Segment_F <= r_Hex_Encoding_state[1];
-    r_Segment_G <= r_Hex_Encoding_state[0];
+      3'b111 : begin
+        // Set the period value with the input value as the divisor
+        period <= CLOCKSPEED >>> i_value;
+      end
+    endcase
   end
 
 
